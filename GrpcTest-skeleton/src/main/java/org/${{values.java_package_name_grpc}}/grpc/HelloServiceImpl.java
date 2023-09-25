b494970c870ef23682434ee9bc44a554ba692aec
Dipti Tawari <dipti_tiwari@naxgrp.com>
2023-09-25 14:07:31 +0530

    commit

--- GrpcTest-skeleton/src/main/java/org/${{values.java_package_name_grpc}}/grpc/HelloServiceImpl.java
+++ GrpcTest-skeleton/src/main/java/org/${{values.java_package_name_grpc}}/grpc/HelloServiceImpl.java
@@ -5,11 +5,11 @@ import org.lognet.springboot.grpc.GRpcService;
 import org.springframework.stereotype.Service;
 
 @GRpcService
-public class HelloServiceImpl extends com.example.grpc.HelloServiceGrpc.HelloServiceImplBase {
+public class HelloServiceImpl extends com.${{values.java_package_name_grpc}}.grpc.HelloServiceGrpc.HelloServiceImplBase {
     @Override
-    public void sayHello(com.example.grpc.HelloRequest request, StreamObserver<com.example.grpc.HelloResponse> responseObserver) {
+    public void sayHello(com.${{values.java_package_name_grpc}}.grpc.HelloRequest request, StreamObserver<com.${{values.java_package_name_grpc}}.grpc.HelloResponse> responseObserver) {
         String greeting = "Hello, " + request.getName() + "!!!!!";
-        com.example.grpc.HelloResponse response = com.example.grpc.HelloResponse.newBuilder().setGreeting(greeting).build();
+        com.${{values.java_package_name_grpc}}.grpc.HelloResponse response = com.${{values.java_package_name_grpc}}.grpc.HelloResponse.newBuilder().setGreeting(greeting).build();
         responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
